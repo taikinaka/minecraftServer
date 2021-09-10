0f14cb327cb9bef45db96814c4f56d18adc3da49
WATERMELONPOP <xxtobynxx@gmail.com>
2021-09-09 18:06:49 -0700

    created arena & rainbow wool

--- src/me/spigot/server/Main.java
+++ src/me/spigot/server/Main.java
@@ -10,10 +10,16 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
+
 public class Main extends JavaPlugin{
+	public Playermove pm;
 	@Override
 	public void onEnable() {
-		getServer().getPluginManager().registerEvents(new Playermove(), this);
+		pm=new Playermove();
+		getServer().getPluginManager().registerEvents(pm, this);
 	}
 	//Test Test Test Test TestTest 
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
@@ -67,6 +73,9 @@ public class Main extends JavaPlugin{
 				return true;
 			}
 		}
+		else if (label.equalsIgnoreCase("canwalk")) {
+			pm.canwalk=!pm.canwalk;
+		}
 		else if (label.equalsIgnoreCase("clear_world")) {
 			if (sender instanceof Player) {
 				Player player = (Player) sender;
@@ -91,6 +100,51 @@ public class Main extends JavaPlugin{
 				return true;
 			}
 		}
+		else if (label.equalsIgnoreCase("lava_game_start")) {
+			if (sender instanceof Player) {
+				Player player = (Player) sender;
+				Location loc = player.getLocation();
+				World targetWorld = loc.getWorld();
+				
+				player.sendTitle("Welcome, "+ChatColor.AQUA+player.getDisplayName(), "Hello", 10, 70, 20);
+				player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent("Test"));
+				
+				
+				
+				
+				for (int x=0; x<40;x++) {
+					for (int z=0; z<40;z++) {
+						if (x<20) {
+							if (z<20) {
+								targetWorld.getBlockAt(loc.getBlockX()+x-20,loc.getBlockY()-1,loc.getBlockZ()+z-20).setType(Material.BLUE_WOOL);
+							}
+							else if (z>20) {
+								targetWorld.getBlockAt(loc.getBlockX()+x-20,loc.getBlockY()-1,loc.getBlockZ()+z-20).setType(Material.RED_WOOL);
+							}
+							else {
+								targetWorld.getBlockAt(loc.getBlockX()+x-20,loc.getBlockY()-1,loc.getBlockZ()+z-20).setType(Material.BLACK_WOOL);
+							}
+						}
+						else if (x>20) {
+							if (z<20) {
+								targetWorld.getBlockAt(loc.getBlockX()+x-20,loc.getBlockY()-1,loc.getBlockZ()+z-20).setType(Material.GREEN_WOOL);
+							}
+							else if (z>20) {
+								targetWorld.getBlockAt(loc.getBlockX()+x-20,loc.getBlockY()-1,loc.getBlockZ()+z-20).setType(Material.YELLOW_WOOL);
+							}
+							else {
+								targetWorld.getBlockAt(loc.getBlockX()+x-20,loc.getBlockY()-1,loc.getBlockZ()+z-20).setType(Material.BLACK_WOOL);
+							}
+						}
+						else {
+							targetWorld.getBlockAt(loc.getBlockX()+x-20,loc.getBlockY()-1,loc.getBlockZ()+z-20).setType(Material.BLACK_WOOL);
+						}
+					}
+				}
+				return true;
+			}
+		}
+		
 		
 		
 		
--- src/me/spigot/server/Playermove.java
+++ src/me/spigot/server/Playermove.java
@@ -1,5 +1,7 @@
 package me.spigot.server;
 
+import java.util.Random;
+
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
@@ -9,15 +11,36 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 
 public class Playermove implements Listener {
+	public boolean canwalk;
 	@EventHandler
 	public void onPlayerMove (PlayerMoveEvent event) {
+		Material[] wools = {Material.BLACK_WOOL, 
+							Material.BLUE_WOOL,
+							Material.BROWN_WOOL,
+							Material.CYAN_WOOL,
+							Material.GRAY_WOOL,
+							Material.GREEN_WOOL,
+							Material.LIGHT_BLUE_WOOL,
+							Material.LIGHT_GRAY_WOOL,
+							Material.LIME_WOOL,
+							Material.MAGENTA_WOOL,
+							Material.ORANGE_WOOL,
+							Material.PINK_WOOL,
+							Material.PURPLE_WOOL,
+							Material.RED_WOOL,
+							Material.YELLOW_WOOL,
+							Material.WHITE_WOOL};
+		int rand = new Random().nextInt(wools.length);
+		Material rWool = wools[rand];
+		
 		Player player = event.getPlayer();
 		Location loc = player.getLocation();
 		loc.setY(loc.getY()-1);
 		World targetWorld = loc.getWorld();
 		String blockName = loc.getBlock().getType().toString();
-		if (blockName.equals("AIR")) {
-			targetWorld.getBlockAt(loc.getBlockX(),loc.getBlockY(),loc.getBlockZ()).setType(Material.IRON_BLOCK);
+		if (blockName.equals("AIR") && canwalk) {
+			targetWorld.getBlockAt(loc.getBlockX(),loc.getBlockY(),loc.getBlockZ()).setType(rWool);
+				
 		}
 	}
 }
